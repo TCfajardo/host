7369220f387eacf04fc5cac728adb4caac2ff990
Catalina Fajardo <tania.fajardo01@uptc.edu.co>
2024-04-02 08:07:28 -0500

    url deploy and dev mode

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,9 @@ const Dotenv = require('dotenv-webpack');
 const deps = require("./package.json").dependencies;
 module.exports = (_, argv) => ({
   output: {
-    publicPath: "http://localhost:3000/",
+    publicPath: argv.mode === "development" 
+                ? "http://localhost:3000/" 
+                : "https://astonishing-piroshki-c9be1c.netlify.app/"
   },
 
   resolve: {
